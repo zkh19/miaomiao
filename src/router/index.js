0adc8f5d9e4a7ebf55b8713af0ddd37bdd6a9068
zkh19 <346417656@qq.com>
2020-11-25 19:49:58 +0800

    modified:   src/App.vue 	new file:   src/router/cinema/index.js 	modified:   src/router/index.js 	new file:   src/router/mine/index.js 	new file:   src/router/movie/index.js 	new file:   src/views/Cinema/index.vue 	new file:   src/views/Mine/index.vue 	new file:   src/views/Movie/index.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+
+import movieRouter from './movie'
+import cinemaRouter from './cinema'
+import mineRouter from './mine'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+  movieRouter,
+  cinemaRouter,
+  mineRouter
 ]
 
 const router = createRouter({
@@ -23,3 +29,11 @@ const router = createRouter({
 })
 
 export default router
+
+// export default new Router({
+//   mode: 'history',
+//   base: process,env,BASE_UTL,
+//   routers:[
+//     movieRouter
+//   ]
+// })
